b2be3c8777ddb0f191ff10ec248b14370b4447b3
MarcusAfolabi <biormarcus@gmail.com>
2026-03-12 04:34:13 +0300

    WIP

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import  { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Check, Zap, ShieldCheck, Building2 } from "lucide-react";
 
